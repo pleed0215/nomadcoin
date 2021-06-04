09a1add2a424d79d2bc49657d8f5e706e8229d4b
Lee Eun Deok <pleed0215@gmail.com>
2021-06-04 21:03:24 +0900

    졸려서...

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-
-	Person "github.com/pleed0215/nomadcoin/person"
 )
 
+type block struct {
+	data string
+	hash string
+	prevHash string
+}
 
 
 
 func main() {
-	led := Person.Person{Name: "Eundeok", Age: 1}
-	fmt.Println(led.SayHello())
-	led.AddAge()
-	fmt.Println(led)
+	genesisBlock := block{"Genesis block", "", ""}
+	genesisBlock.hash = fmt.Sprintf("%x", sha256.Sum256([]byte(genesisBlock.data+genesisBlock.prevHash)))
+
+	secondBlock := block{"Second block", "", genesisBlock.hash}
+	secondBlock.hash = fmt.Sprintf("%x", sha256.Sum256([]byte(secondBlock.data+secondBlock.prevHash)))
 }
